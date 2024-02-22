aa10e37e7ebc24c6307b32e35caf2875b1af6bb0
Fahad warsi <fahad7swarsi@gmail.com>
2024-02-22 08:02:49 +0500

    code file of typecript series

---
+++ 01intro/varibleme.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var greeting = "Hello, World!";
+console.log(greeting.toUpperCase());
+var userId = 354236.555;
+userId.toFixed();
+console.log(userId.toFixed());
+var hero;
+function getHero() {
+    return "thor";
+}
+hero = getHero();
+console.log(hero);
---
+++ 01intro/varibleme.ts
@@ -0,0 +1,17 @@
+let greeting: string = "Hello, World!";
+
+console.log(greeting.toUpperCase());
+
+let userId = 354236.555;
+userId.toFixed();
+console.log(userId.toFixed());
+
+let hero: string;
+function getHero() {
+  return "thor";
+}
+
+hero = getHero();
+console.log(hero);
+
+export {};
